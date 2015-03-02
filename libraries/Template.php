72bbeaec7fb6df6087fd114311ffa86f9ccd2095
Nezar Fadle <nezarfadle@gmail.com>
2015-03-02 18:03:20 +0800

    Add Template Library

---
+++ libraries/Template.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Template {
+
+	public function view($tpl, $data = null, $returnView = false){
+		
+		if(file_exists(APPPATH . '/views/' . $tpl . '.php')){
+
+			if($data !== null && is_array($data)){
+				extract($data);
+			}
+
+			ob_start();
+			include APPPATH . 'views/' . $tpl . '.php';
+
+			if(!$returnView){
+				ob_end_flush();
+			}else{
+				$buffer = ob_get_clean();
+				return $buffer;
+			}
+			
+		}
+
+	}
+	
+}
